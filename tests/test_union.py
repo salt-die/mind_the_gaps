ae91671db88db411ab9943f0648a121d75771d59
salt-die <53280662+salt-die@users.noreply.github.com>
2023-11-26 04:10:04 -0600

    Add additional union tests.

--- tests/test_union.py
+++ tests/test_union.py
@@ -1,10 +1,31 @@
 from mind_the_gaps import Endpoint, Gaps
 
 
-def test_bounded_disjoint_a_and_b():
-    a = Gaps([Endpoint(0, "["), Endpoint(1, "]")])
-    b = Gaps([Endpoint(2, "["), Endpoint(3, "]")])
+def test_bounded_disjoint():
+    a = Gaps([0, 1])
+    b = Gaps([2, 3])
+    assert a | b == Gaps([0, 1, 2, 3])
 
-    assert a | b == Gaps(
-        [Endpoint(0, "["), Endpoint(1, "]"), Endpoint(2, "["), Endpoint(3, "]")]
-    )
+
+def test_unbounded_disjoint():
+    a = Gaps([-float("inf"), 0])
+    b = Gaps([1, 2])
+    assert a | b == Gaps([-float("inf"), 0, 1, 2])
+
+
+def test_bounded_intersecting_proper():
+    a = Gaps([0, 2])
+    b = Gaps([1, 3])
+    assert a | b == Gaps([0, 3])
+
+
+def test_bounded_intersecting_singleton():
+    a = Gaps([0, 2])
+    b = Gaps([1, 1])
+    assert a | b == a
+
+
+def test_bounded_intersecting_empty():
+    a = Gaps([0, 2])
+    b = Gaps([Endpoint(0, "("), Endpoint(0, "]")])
+    assert a | b == a
