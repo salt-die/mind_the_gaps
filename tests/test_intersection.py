6192f85ecdc20814fd917cd719b697dd0e75f6e8
salt-die <53280662+salt-die@users.noreply.github.com>
2023-11-26 04:50:03 -0600

    Add additional tests.

--- tests/test_intersection.py
+++ tests/test_intersection.py
@@ -4,28 +4,28 @@ from mind_the_gaps import Gaps
 def test_bounded_disjoint():
     a = Gaps([0, 1])
     b = Gaps([2, 3])
-    assert a | b == Gaps([0, 1, 2, 3])
+    assert a & b == Gaps()
 
 
 def test_unbounded_disjoint():
     a = Gaps([-float("inf"), 0])
     b = Gaps([1, 2])
-    assert a | b == Gaps([-float("inf"), 0, 1, 2])
+    assert a & b == Gaps()
 
 
 def test_bounded_intersecting_proper():
     a = Gaps([0, 2])
     b = Gaps([1, 3])
-    assert a | b == Gaps([0, 3])
+    assert a & b == Gaps([1, 2])
 
 
 def test_bounded_intersecting_singleton():
     a = Gaps([0, 2])
     b = Gaps([1, 1])
-    assert a | b == a
+    assert a & b == b
 
 
-def test_empty_union():
+def test_empty_intersection():
     a = Gaps([0, 2])
     b = Gaps()
-    assert a | b == a
+    assert a & b == b
--- tests/test_xor.py
+++ tests/test_xor.py
@@ -1,31 +1,31 @@
-from mind_the_gaps import Gaps
+from mind_the_gaps import Endpoint, Gaps
 
 
 def test_bounded_disjoint():
     a = Gaps([0, 1])
     b = Gaps([2, 3])
-    assert a | b == Gaps([0, 1, 2, 3])
+    assert a ^ b == Gaps([0, 1, 2, 3])
 
 
 def test_unbounded_disjoint():
     a = Gaps([-float("inf"), 0])
     b = Gaps([1, 2])
-    assert a | b == Gaps([-float("inf"), 0, 1, 2])
+    assert a ^ b == Gaps([-float("inf"), 0, 1, 2])
 
 
 def test_bounded_intersecting_proper():
     a = Gaps([0, 2])
     b = Gaps([1, 3])
-    assert a | b == Gaps([0, 3])
+    assert a ^ b == Gaps([0, Endpoint(1, ")"), Endpoint(2, "("), 3])
 
 
 def test_bounded_intersecting_singleton():
     a = Gaps([0, 2])
     b = Gaps([1, 1])
-    assert a | b == a
+    assert a ^ b == Gaps([0, Endpoint(1, ")"), Endpoint(1, "("), 2])
 
 
-def test_empty_union():
+def test_empty_xor():
     a = Gaps([0, 2])
     b = Gaps()
-    assert a | b == a
+    assert a ^ b == a
