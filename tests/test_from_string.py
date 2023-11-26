d0ea6afe4f76433b59583ba5ce90702d455ecac2
salt-die <53280662+salt-die@users.noreply.github.com>
2023-11-26 14:00:02 -0600

    Reversed the arguments to equals in asserts.

--- tests/test_from_string.py
+++ tests/test_from_string.py
@@ -2,24 +2,24 @@ from mind_the_gaps import Endpoint, Gaps
 
 
 def test_from_string_closed():
-    assert Gaps([0, 1, 2, 3]) == Gaps.from_string("{[0, 1], [2, 3]}")
+    assert Gaps.from_string("{[0, 1], [2, 3]}") == Gaps([0, 1, 2, 3])
 
 
 def test_from_string_open():
-    assert Gaps(
+    assert Gaps.from_string("{(0, 1), (2, 3)}") == Gaps(
         [Endpoint(0, "("), Endpoint(1, ")"), Endpoint(2, "("), Endpoint(3, ")")]
-    ) == Gaps.from_string("{(0, 1), (2, 3)}")
+    )
 
 
 def test_from_string_half_open():
-    assert Gaps(
+    assert Gaps.from_string("{(-inf, 0], [1, 2)}") == Gaps(
         [Endpoint(-float("inf"), "("), 0, 1, Endpoint(2, ")")]
-    ) == Gaps.from_string("{(-inf, 0], [1, 2)}")
+    )
 
 
 def test_from_string_singleton():
-    assert Gaps([0, 0]) == Gaps.from_string("{[0, 0]}")
+    assert Gaps.from_string("{[0, 0]}") == Gaps([0, 0])
 
 
 def test_from_string_empty():
-    assert Gaps() == Gaps.from_string("{}")
+    assert Gaps.from_string("{}") == Gaps()
