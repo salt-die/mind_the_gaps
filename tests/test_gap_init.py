d575677f770cf27c89d8d4193b9adfe6aa28f858
salt-die <53280662+salt-die@users.noreply.github.com>
2023-11-27 04:26:13 -0600

    Add gap init test.

---
+++ tests/test_gap_init.py
@@ -0,0 +1,25 @@
+import pytest
+from mind_the_gaps import Endpoint, Gaps
+
+
+def test_unsorted_closed():
+    with pytest.raises(ValueError, match=r"unsorted"):
+        Gaps([1, 0])
+
+
+def test_unsorted_open():
+    with pytest.raises(ValueError, match=r"unsorted"):
+        Gaps([Endpoint(1, "("), Endpoint(0, ")")])
+
+
+def test_singleton_ok():
+    Gaps([1, 1])
+
+
+def test_missing_singleton_ok():
+    Gaps([0, Endpoint(1, ")"), Endpoint(1, "("), 2])
+
+
+def test_open_closed():
+    with pytest.raises(ValueError, match=r"not minimally expressed"):
+        Gaps([0, Endpoint(1, ")"), 1, 2])
