ec4e8685ea25db94ef64050818f4198fe81a8aa1
salt-die <salt-die@protonmail.com>
2025-02-11 18:25:48 -0600

    Fix one of the wrong boundary tests.

--- tests/test_gap_init.py
+++ tests/test_gap_init.py
@@ -11,6 +11,13 @@ def test_missing_singleton_ok():
     Gaps([0, Endpoint(1, ")"), Endpoint(1, "("), 2])
 
 
+def test_fix_not_minimally_expressed():
+    assert Gaps([0, Endpoint(1, ")"), 1, 2]) == Gaps([0, 2])
+    assert Gaps([0, 1, Endpoint(1, "("), 2]) == Gaps([0, 2])
+    assert Gaps([0, 0, 0, 0]) == Gaps([0, 0])
+    assert Gaps([1, 2, 2, 3]) == Gaps([1, 3])
+
+
 def test_unsorted_closed():
     with pytest.raises(ValueError, match="unsorted"):
         Gaps([1, 0])
@@ -19,13 +26,6 @@ def test_unsorted_closed():
 def test_unsorted_open():
     with pytest.raises(ValueError, match="unsorted"):
         Gaps([Endpoint(1, "("), Endpoint(0, ")")])
-
-
-def test_fix_not_minimally_expressed():
-    assert Gaps([0, Endpoint(1, ")"), 1, 2]) == Gaps([0, 2])
-    assert Gaps([0, 1, Endpoint(1, "("), 2]) == Gaps([0, 2])
-    assert Gaps([0, 0, 0, 0]) == Gaps([0, 0])
-    assert Gaps([1, 2, 2, 3]) == Gaps([1, 3])
 
 
 def test_unsorted_open_closed():
@@ -43,24 +43,18 @@ def test_unsorted_open_open():
         Gaps([Endpoint(1, "("), Endpoint(1, ")")])
 
 
-def test_wrong_boundary_left_closed():
+def test_wrong_boundary():
     with pytest.raises(ValueError, match="left"):
         Gaps([Endpoint(0, "]"), 1])
 
-
-def test_wrong_boundary_left_open():
     with pytest.raises(ValueError, match="left"):
         Gaps([Endpoint(0, ")"), 1])
 
-
-def test_wrong_boundary_right_closed():
     with pytest.raises(ValueError, match="right"):
         Gaps([0, Endpoint(1, "[")])
 
-
-def test_wrong_boundary_right_open():
     with pytest.raises(ValueError, match="right"):
-        Gaps([0, Endpoint(1, "[")])
+        Gaps([0, Endpoint(1, "(")])
 
 
 def test_wrong_number_of_endpoints():
